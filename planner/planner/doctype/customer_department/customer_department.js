830c14dd5cf2dcac3dc475a74e1f06dfeeedcfe8
Frappe User <frappeuser@instance-20250810-133016.us-central1-c.c.ambient-scope-462704-u2.internal>
2025-08-19 03:41:15 +0000

    customer department added

---
+++ planner/planner/doctype/customer_department/customer_department.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Kebazz Technologies and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Customer Department", {
+// 	refresh(frm) {
+
+// 	},
+// });
